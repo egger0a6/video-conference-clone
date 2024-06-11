c76facf41c7a26e99e5f6da7e4f10e8ed63835f5
Zach Eggert <egger0a6@email.arizona.edu>
2024-06-11 00:08:55 -0400

    change width styling of HomeCard component

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -18,7 +18,7 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
     >
       <div
         onClick={handleClick}
-        className={cn(`px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px]`, className)}
+        className={cn(`px-4 py-6 flex flex-col justify-between w-full min-h-[260px] rounded-[14px]`, className)}
       >
         <div className="flex-center glassmorphism size-14 rounded-[10px]">
           <Image
